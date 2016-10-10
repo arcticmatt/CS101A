a2e40ee081595bf8e57536cb8249dad252c0eafe
Kshitij Grover <kgrover@caltech.edu>
2016-10-10 14:38:20 -0700

    Added binary song data to get_songs script

--- get_songs.py
+++ get_songs.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import base64
 
 starting_offset = 0
 
@@ -12,7 +13,7 @@ class Album:
 
 
 # Used to store Spotify song information.
-# Genres is currently not populated for a lot (basically any) albums. We 
+# Genres is currently not populated for a lot (basically any) albums. We
 # won't actually use it for now.
 class Song:
     def __init__(self, song_id, name, year, popularity, preview_url, genres):
@@ -22,6 +23,14 @@ class Song:
         self.popularity = popularity
         self.preview_url = preview_url
         self.genres = genres
+        # base 64 encoded data
+        self.binary_data = self.get_binary_song_data(preview_url)
+
+    def get_binary_song_data(self, url):
+        r = requests.get(url)
+        # binary data
+        song_data = r.content
+        return base64.b64encode(song_data)
 
 
 def create_song(song_req, album_obj):
@@ -29,7 +38,7 @@ def create_song(song_req, album_obj):
     song_id = song_dict['id']
     song_name = song_dict['name']
     song_year = album_obj.date.split('-')[0]
-    song_popularity = song_dict['popularity'] 
+    song_popularity = song_dict['popularity']
     song_preview_url = song_dict['preview_url']
     return Song(song_id, song_name, song_year, song_popularity, song_preview_url, album_obj.genres)
 
@@ -48,10 +57,10 @@ def get_songs(year, starting_offset, num_songs=100):
     req_str = 'https://api.spotify.com/v1/search?q=year:{0}&type=album&offset={1}'.format(year, starting_offset)
     req = requests.get(req_str)
 
-    while songs_proc < num_songs: 
+    while songs_proc < num_songs:
         print '{} albums processed, {} songs processed...'.format(albums_proc, songs_proc)
         # List of 20 albums. Each item is a dictionary containing some album
-        # information (album_type, available_markets, external_urls, href, id, 
+        # information (album_type, available_markets, external_urls, href, id,
         # images, name, type, uri). The item does not contain the full album
         # information (release date, genres, etc.). However, the href
         # can be used to fetch that.
@@ -67,7 +76,7 @@ def get_songs(year, starting_offset, num_songs=100):
             for song in songs:
                 print 'Processing songs for album {}'.format(album_req.json()['name'].encode('ascii', 'ignore'))
                 if song['preview_url']:
-                    # Get song information (so we can get song popularity). 
+                    # Get song information (so we can get song popularity).
                     # Note that a list of genres is given in the album_req.
                     album_obj = create_album(album_req)
                     song_req = requests.get(song['href'])
@@ -88,7 +97,7 @@ def get_songs(year, starting_offset, num_songs=100):
 
 # We'll use albums from the 60's to the 00's
 years = range(1960, 1961)
-songs_per_year = 10
+songs_per_year = 2
 print 'Getting {} songs from {}'.format(songs_per_year * len(years), years)
 songs = []
 for year in years:
@@ -99,5 +108,5 @@ print 'Writing data to CSV...'
 with open('song_data.csv', 'w+') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for song in songs:
-        writer.writerow([song.song_id, song.name, song.year, song.popularity, 
-                         song.preview_url])
+        writer.writerow([song.song_id, song.name, song.year, song.popularity,
+                         song.preview_url, song.binary_data])
