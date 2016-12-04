07854fb58febff2ecb1062e7132a37bcbbe33de6
Ubuntu <ubuntu@ip-172-31-43-250.us-west-2.compute.internal>
2016-12-04 23:00:31 +0000

    Changing Params

--- gpu/cifar/cifar10.py
+++ gpu/cifar/cifar10.py
@@ -65,17 +65,17 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
 # Some network parameters
 # TODO(smurching): Make these flags?
-NUM_CONV_LAYERS = 4
+NUM_CONV_LAYERS = 6
 NUM_RECC_LAYERS = 2
 CONV_FILTER_SIZE = [3, 3]
-MAX_POOL_KERNEL_SIZE = [[2, 2], [3, 3], [4, 4], [4, 4]]
+MAX_POOL_KERNEL_SIZE = [[2, 2], [2, 2], [2, 2], [3, 3], [4, 4], [4, 4]]
 RECC_LAYER_SIZE = 30
 
 # Constants describing the training process.
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
 NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 0.0005       # Initial learning rate.
+INITIAL_LEARNING_RATE = 0.001     # Initial learning rate.
 
 def _activation_summary(x):
   """Helper to create summaries for activations.
@@ -156,14 +156,18 @@ def build_conv_layers(input_tensor, num_layers, filter_size=None):
     # Expects input tensor [batch_size, height, width, in_channels]
     # These dimensions correspond to [batch_size, num_subsamples, num_coeffs, 1] in our case
     with tf.variable_scope('conv_%d'%(i + 1)) as scope:
-      conv = tflearn.layers.conv.conv_2d (final_layer, nb_filter=1, filter_size=filter_size, strides=1,
+      if i >= 2:
+        stride = [1, 3, 3, 1]
+      else:
+        stride = [1, 1, 1, 1]
+      conv = tflearn.layers.conv.conv_2d (final_layer, nb_filter=1, filter_size=filter_size, strides=stride,
         padding='same', activation='linear', bias=True, weights_init='uniform_scaling',
         bias_init='zeros', regularizer=None, weight_decay=0.001,
         restore=True, reuse=None, scope=scope)
 
     # TODO(smurching): Intelligently pick values for the kernel size/stride here
     final_layer = tflearn.layers.conv.max_pool_2d (conv, kernel_size=[1] + MAX_POOL_KERNEL_SIZE[i] + [1],
-      strides=[1, 2, 2, 1], padding='same', name='MaxPool2D_%d'%i)
+      strides=[1, 1, 1, 1], padding='same', name='MaxPool2D_%d'%i)
   return final_layer
 
 def build_recurrent_layers(input_tensor, num_layers, units_per_layer=3, activation='sigmoid', dropout=0.8):
