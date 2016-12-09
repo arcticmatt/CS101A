Fix neural net code to output num_classes logits instead of arbitrarily outputting the same number of logits as units in the final recurrent layer. WIP adding eval calculation to training

gpu/cifar/cifar10.py
@@ -76,14 +76,21 @@ tf.app.flags.DEFINE_integer('num_subsamples', 1324,
 tf.app.flags.DEFINE_integer('num_coeffs', 100,
                             """Number of MFCC coefficients""")
 
+tf.app.flags.DEFINE_integer('eval_batch_size', 100,
+                            """Number of examples to run during each 
+                            batch of evaluation.""")
+
 tf.app.flags.DEFINE_string('train_data', None, 'Training data HDF5 file')
 
-tf.app.flags.DEFINE_integer('num_conv_layers', 6,
+tf.app.flags.DEFINE_integer('num_conv_layers', 5,
                             """How many Convolution Layers to use.""")
 tf.app.flags.DEFINE_integer('num_recc_layers', 2,
                             """How many Recurrent Layers to use.""")
 tf.app.flags.DEFINE_integer('recc_layer_size', 30,
                             """Number of units in recc layer""")
+
+tf.app.flags.DEFINE_integer('num_classes', 4,
+                            """Number of classes for classification""")
 
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
@@ -100,7 +107,7 @@ RECC_LAYER_SIZE = FLAGS.recc_layer_size
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
 NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 0.001     # Initial learning rate.
+INITIAL_LEARNING_RATE = 0.002     # Initial learning rate.
 
 def _activation_summary(x):
   """Helper to create summaries for activations.
@@ -200,9 +207,6 @@ def build_recurrent_layers(input_tensor, num_layers, units_per_layer=3, activati
   for i in xrange(num_layers):
     is_last_layer = (i == num_layers - 1)
     if is_last_layer:
-      # We don't apply softmax for the last layer because
-      # tf.nn.sparse_softmax_cross_entropy_with_logits accepts the unscaled logits
-      # and performs the softmax internally for efficiency.
       curr_activation = 'linear'
     else:
       curr_activation = activation
@@ -219,7 +223,6 @@ def build_recurrent_layers(input_tensor, num_layers, units_per_layer=3, activati
         final_layer = tf.pack(final_layer, axis=1)
 
   return final_layer
-
 
 def inference(songs):
   """Build the CIFAR-10 model.
@@ -248,8 +251,17 @@ def inference(songs):
   conv_reshaped = tf.squeeze(convolutional_layers, squeeze_dims=[3])
   print("RNN input shape (batch_size x timesteps x num_coeffs): %s"%conv_reshaped.get_shape())
 
-  return build_recurrent_layers(conv_reshaped, num_layers=NUM_RECC_LAYERS,
+  recurrent_layers = build_recurrent_layers(conv_reshaped, num_layers=NUM_RECC_LAYERS,
     units_per_layer=RECC_LAYER_SIZE, activation='sigmoid')
+
+  # Add a linear activation (Wx + b) layer
+  # We don't apply softmax for the last layer because
+  # tf.nn.sparse_softmax_cross_entropy_with_logits accepts the unscaled logits
+  # and performs the softmax internally for efficiency.  
+  with tf.variable_scope('activation_layer') as scope:
+    result = tflearn.layers.core.fully_connected(recurrent_layers, n_units=FLAGS.num_classes,
+      activation='linear', bias=True, reuse=None, scope=scope)
+  return result
 
 def loss(logits, labels):
   """Add L2Loss to all the trainable variables.

gpu/cifar/cifar10_eval.py
@@ -43,6 +43,7 @@ import tensorflow as tf
 
 import cifar10
 import train_utils
+import hdf5_reader
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -54,23 +55,12 @@ tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/cifar10_train',
                            """Directory where to read model checkpoints.""")
 tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
                             """How often to run the eval.""")
-tf.app.flags.DEFINE_integer('num_examples', 10000,
-                            """Number of examples to run.""")
 tf.app.flags.DEFINE_boolean('run_once', False,
                          """Whether to run eval only once.""")
 
-# TODO(smurching): Remove extra flags
-# TODO(smurching): Assert this while reading training examples
-tf.app.flags.DEFINE_integer('num_subsamples', 1324, 
-                            """Number of sampled values for each MFCC coefficient""")
 
-tf.app.flags.DEFINE_integer('num_coeffs', 100, 
-                            """Number of MFCC coefficients""")
-
-tf.app.flags.DEFINE_string('train_data', None, 'Training data CSV')
-
-READER = train_utils.HDF5BatchProcessor(filename=FLAGS.train_data,
-  batch_size=FLAGS.batch_size)
+READER = hdf5_reader.HDF5Reader(filename=FLAGS.train_data,
+  batch_size=FLAGS.batch_size, eval_batch_size=FLAGS.eval_batch_size)
 
 def eval_once(saver, top_k_op, feed_dict):
   """Run Eval once.
@@ -82,62 +72,85 @@ def eval_once(saver, top_k_op, feed_dict):
   """
   with tf.Session() as sess:
     ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
-    if ckpt and ckpt.model_checkpoint_path:
+    if ckpt and ckpt.model_checkpoint_path and saver is not None:
       # Restores from checkpoint
       saver.restore(sess, ckpt.model_checkpoint_path)
-      # Assuming model_checkpoint_path looks something like:
-      #   /my-favorite-path/cifar10_train/model.ckpt-0,
-      # extract global_step from it.
-      global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+    elif saver is None:
+      print('Not restoring model from checkpoint before eval: saver passed to eval_once is None')
     else:
-      print('No checkpoint file found')
+      print('Failed to restore model from checkpoint for eval: no checkpoint file found')
       return
 
+    # Evaluate on all the eval set points
+    total_sample_count = FLAGS.eval_batch_size
+    predictions = sess.run([top_k_op], feed_dict=feed_dict)
+    true_count = np.sum(predictions) # Counts the number of correct predictions.
+
     # Begin evaluation on FLAGS.num_examples training points
-    num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
+    num_iter = int(math.ceil(READER.num_eval_rows / FLAGS.batch_size))
+    print("Taking %s iters to eval on full set"%num_iter)
     true_count = 0  # Counts the number of correct predictions.
     total_sample_count = num_iter * FLAGS.batch_size
-    step = 0
 
-    while step < num_iter:
+    for step in xrange(num_iter):
       predictions = sess.run([top_k_op], feed_dict=feed_dict)
       true_count += np.sum(predictions)
-      step += 1
 
     # Compute precision @ 1.
     precision = true_count / total_sample_count
     print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
+    return precision
 
-def build_feed_dict(reader, features_placeholder, label_placeholder):
+def build_eval_dict(reader, features_placeholder, label_placeholder):
   result = {}
-  features, labels = train_utils.inputs(reader)
+  features, labels = train_utils.get_eval_batch(reader)
   result[features_placeholder] = features
   result[label_placeholder] = labels
   return result
 
-def evaluate():
+def build_eval_graph():
+  '''
+  Adds ops to the current graph for evaluating out-of-sample error.
+
+  Returns a tuple (features_placeholder, label_placeholder, eval_op) comprising
+  the input placeholders for evaluation, along with the op for computing
+  error.
+  '''
+    # Get feature and label placeholders
+  features_placeholder = tf.placeholder(tf.float32, shape=[None,
+    FLAGS.num_subsamples, FLAGS.num_coeffs, 1])
+  label_placeholder = tf.placeholder(tf.int32, shape=[None,])
+
+  # Build a Graph that computes the logits predictions from the
+  # inference model.
+  logits = cifar10.inference(features_placeholder)
+  
+  # print("Predictions size: %s"%logits.get_shape())
+  # logits = tf.Print(logits, [logits], "Logits: ", summarize=10)
+  # label_print = tf.Print(label_placeholder, [label_placeholder], "Labels: ", summarize=10)
+
+  # Calculate predictions.
+  top_k_op = tf.nn.in_top_k(tf.nn.softmax(logits), label_placeholder, 1)
+  return (features_placeholder, label_placeholder, top_k_op) 
+
+def evaluate(restore=True):
   """Eval CIFAR-10 for a number of steps."""
   with tf.Graph().as_default() as g:
-    # Get feature and label placeholders
-    features_placeholder = tf.placeholder(tf.float32, shape=[FLAGS.batch_size,
-      FLAGS.num_subsamples, FLAGS.num_coeffs, 1])
-    label_placeholder = tf.placeholder(tf.int32, shape=[FLAGS.batch_size,])
-
-    # Build a Graph that computes the logits predictions from the
-    # inference model.
-    logits = cifar10.inference(features_placeholder)
-
-    # Calculate predictions.
-    top_k_op = tf.nn.in_top_k(logits, label_placeholder, 1)
+    
+    # Build the eval graph
+    features_placeholder, label_placeholder, top_k_op = build_eval_graph()
 
     # Restore the moving average version of the learned variables for eval.
-    variable_averages = tf.train.ExponentialMovingAverage(
-        cifar10.MOVING_AVERAGE_DECAY)
-    variables_to_restore = variable_averages.variables_to_restore()
-    saver = tf.train.Saver(variables_to_restore)
+    if restore:
+      variable_averages = tf.train.ExponentialMovingAverage(
+          cifar10.MOVING_AVERAGE_DECAY)
+      variables_to_restore = variable_averages.variables_to_restore()
+      saver = tf.train.Saver(variables_to_restore)
+    else:
+      saver = None
 
     while True:
-      feed_dict = build_feed_dict(READER, features_placeholder, label_placeholder)
+      feed_dict = build_eval_dict(READER, features_placeholder, label_placeholder)
       eval_once(saver, top_k_op, feed_dict)
       if FLAGS.run_once:
         break

gpu/cifar/cifar10_multi_gpu_train.py
@@ -51,13 +51,14 @@ import tensorflow as tf
 import cifar10
 
 import train_utils
+import hdf5_reader
 
 import freeze_graph
 
 FLAGS = tf.app.flags.FLAGS
 
-READER = train_utils.HDF5BatchProcessor(filename=FLAGS.train_data,
-  batch_size=FLAGS.batch_size)
+READER = hdf5_reader.HDF5Reader(filename=FLAGS.train_data,
+  batch_size=FLAGS.batch_size, eval_batch_size=FLAGS.eval_batch_size)
 
 FEATURE_PLACEHOLDER_NAME = "FEATURES_PLACEHOLDER"
 LABEL_PLACEHOLDER_NAME = "LABEL_PLACEHOLDER"
@@ -195,7 +196,6 @@ def train():
     # Set up dict mapping GPU scopes to placeholders
     placeholder_dict = build_placeholder_dict()
 
-
     # Calculate the gradients for each model tower.
     tower_grads = []
     print("Num gpus = {}".format(FLAGS.num_gpus))
@@ -300,60 +300,9 @@ def train():
         summary_writer.add_summary(summary_str, step)
 
       # Save the model checkpoint periodically and freeze the graph.
-      if step % 2000 == 0 or (step + 1) == FLAGS.max_steps:
+      if step % 10 == 0 or (step + 1) == FLAGS.max_steps:
         checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
         saver.save(sess, checkpoint_path, global_step=step)
-        # # Save model checkpoint.
-        # print('Saving model checkpoint...')
-        # checkpoint_prefix = os.path.join(FLAGS.train_dir, 'model.ckpt')
-        # checkpoint_state_name = 'checkpoint_state'
-        # checkpoint_path = saver.save(sess, checkpoint_prefix, global_step=step,
-        #                              latest_filename=checkpoint_state_name)
-
-        # input_graph_name = 'input_graph.pb'
-        # output_graph_name = "output_graph-{}.pb".format(step)
-        # # Save model structure.
-        # print('Saving model structure...')
-        # tf.train.write_graph(sess.graph_def, FLAGS.train_dir, input_graph_name)
-
-        # # Get names of all tensors
-        # names = get_all_tensor_names(sess)
-        # all_names = ','.join(names)
-
-        # # Freeze graph.
-        # input_graph_path = os.path.join(FLAGS.train_dir, input_graph_name)
-        # input_saver_def_path = ''
-        # input_binary = False
-        # output_node_names = all_names
-        # # output_node_names = 'global_step'
-        # restore_op_name = 'save/restore_all'
-        # filename_tensor_name = 'save/Const:0'
-        # output_graph_path = os.path.join(FLAGS.train_dir, output_graph_name)
-        # clear_devices = False
-
-        # print('===== output_node_names = {} ====='.format(output_node_names))
-        # print('Freezing graph...')
-        # s = time.time()
-        # try:
-        #   with tf.Graph().as_default():
-        #     freeze_graph.freeze_graph(input_graph_path, input_saver_def_path,
-        #                               input_binary, checkpoint_path,
-        #                               output_node_names, restore_op_name,
-        #                               filename_tensor_name, output_graph_path,
-        #                               True, '')
-        # except Exception as e:
-        #   print('Freezing graph failed with exception {}'.format(e))
-        # print("Froze graph, took %s sec"%(time.time() - s))
-
-def get_all_tensor_names(sess):
-  names = [n.name for n in sess.graph_def.node if '/' not in n.name and n.name != 'init']
-  print('Number of output nodes = {}'.format(len(names)))
-  return names
-
-def get_all_op_names(sess):
-  op_names = [op.name for op in sess.graph.get_operations() if '/' not in op.name and op.name != 'init']
-  print('Number of op names = {}'.format(len(op_names)))
-  return op_names
 
 def redirect_output():
   prefix = datetime.now().strftime("%b-%d-%y-%I:%M:%S")

gpu/cifar/train_utils.py
@@ -18,42 +18,11 @@ def get_scope_name(tower_idx):
   '''
   return '%s_%d' % (TOWER_NAME, tower_idx)
 
-class HDF5BatchProcessor:
-    '''
-    Reads batches of data from an HDF5 file assumed to contain a root group with the 
-    following keys:
-
-    <LABELS_KEY>: Key for an array of all labels
-    <NUM_ROWS_KEY>: Key for the integer number of rows in the training dataset 
-    <ROW_KEY>_i, for i in xrange(num_rows): Keys for each row in the training dataset
-    <DIMS_KEY>: Key for an array containing the shape of each training point (excluding label)
-    '''
-
-    def __init__(self, filename, batch_size):
-        self.data_file = h5py.File(filename, 'r')
-        self.num_rows = self.data_file[NUM_ROWS_KEY][...][0]
-        self.labels = self.data_file[LABELS_KEY][...]
-        self.batch_size = batch_size
-        self.batch_dims = np.append([batch_size], self.data_file[DIMS_KEY][...])
-        self.row_idx = 0
-
-    def get_batch(self):
-        features = np.zeros(self.batch_dims, dtype='float32')
-        labels = np.zeros(self.batch_size, dtype='float32')
-        # Get <batch_size> lines, splitting each one into a tuple of (features, label)        
-        for i in xrange(self.batch_size):
-            data, label = self.get_row()
-            features[i][...] = data
-            labels[i] = label
-        return (features, labels)
-
-    def get_row(self):
-        row_key = get_key_for_row(self.row_idx)
-        data = self.data_file[row_key][...]
-        label = self.labels[self.row_idx]
-        self.row_idx = (self.row_idx + 1) % self.num_rows
-        return (data, label)
-
+def get_eval_batch(batch_processor):
+  '''
+  Construct batch upon which to evaluate our song classification model.
+  '''
+  return batch_processor.get_eval_batch()
 
 def inputs(batch_processor):
   """Construct input for training our song classification model.
@@ -68,10 +37,4 @@ def inputs(batch_processor):
   Raises:
     ValueError: If no data_dir
   """
-  return batch_processor.get_batch()
-
-if __name__ == "__main__":
-    for i in xrange(10):
-        start = time.time()
-        inputs(batch_processor)
-        print("Got batch in %s seconds"%(time.time() - start))
+  return batch_processor.get_train_batch()